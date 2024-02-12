28a757d45f1fa512689d4d96ee67e363c5f7a178
mozzammelrobi <mozzammelfb@gmail.com>
2024-02-12 23:47:58 +0600

    rectangle calculation area done

--- js/rectangle.js
+++ js/rectangle.js
@@ -1,5 +1,25 @@
 
 
 function rectangleAreaCalculate(){
-    console.log('hellow')
+    // get rectangle length
+   const lengthInput = document.getElementById('rectangle-length')
+//    console.log(lengthInput.value)
+    const lengthText = lengthInput.value 
+    const length = parseFloat(lengthText)
+    console.log(length)
+
+    // get rectangle width
+    const widthInput = document.getElementById('rectangle-width')
+    // console.log(widthInput)
+    const widthText = widthInput.value
+    const width = parseFloat(widthText)
+    console.log(width)
+
+    // calcutate rectangle area
+    const area = length * width;
+    console.log(area)
+
+    // display rectangle area 
+    const rectangleAreaSpan = document.getElementById('rectangle-area')
+    rectangleAreaSpan.innerText = area
 }
